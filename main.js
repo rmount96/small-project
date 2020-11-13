5037ab4dd9b49898ef5bb1afe6e02c334a793e68
Ryan Mount <rmount96@outlook.com>
2020-11-13 09:34:57 -0600

    Final-Version

--- main.js
+++ main.js
@@ -30,43 +30,50 @@ const formHandler = (evt)=>{
 }
 
 const mainContent = document.querySelector('.card-center')
+let button = document.querySelector('button')
     
 ajax("https://quote-garden.herokuapp.com/api/v2/quotes?page=1&limit=20", (r) =>{
     let allQuotes = JSON.parse(r)
     console.log(allQuotes)
 
     let newQuote = () => {
-        if(allQuotes.length != 0) {
             let selectedQuote = allQuotes.quotes.pop();
             let div = document.createElement('div')
             div.className = "card"
             div.style.width = '18rem'
             mainContent.append(div)
+            
+            let closeButton = document.createElement('span')
+            closeButton.innerText = 'X'
+            closeButton.className = "close-btn"
+    
+            closeButton.addEventListener('click', (evt => {
+                let parent = evt.target.parentNode
+                parent.remove();
+
+            }))
+            
+            div.append(closeButton)
 
             let p = document.createElement('p')
             p.append(`"${selectedQuote.quoteText}"`)
             div.append(p)
+            
             let author = document.createElement('h6')
             author.setAttribute("id", "card-author")
             if (selectedQuote.quoteAuthor != false){
-            author.append(`-${selectedQuote.quoteAuthor}`)
-            div.append(author)
+                author.append(`-${selectedQuote.quoteAuthor}`)
+                div.append(author)
             }
             else {
                 author.append("-Unknown")
                 div.append(author)
             }
-
         }
 
-}
-let button = document.querySelector('button')
 button.addEventListener('click', newQuote)
 
-button.onclick = () => {
-    let div = document.createElement('div')
-    div.setAttribute("id", "transition")
-}})
+})
 
 
 
